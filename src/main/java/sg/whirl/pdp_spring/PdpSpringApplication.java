45dbab51c7f9c92983361a54bbe188724754c7a6
m.olimpiiev <m.olimpiiev@whirl.sg>
2019-06-10 20:37:42 +0300

    Commit an empty project.

---
+++ src/main/java/sg/whirl/pdp_spring/PdpSpringApplication.java
@@ -0,0 +1,13 @@
+package sg.whirl.pdp_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdpSpringApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( PdpSpringApplication.class, args );
+    }
+
+}
