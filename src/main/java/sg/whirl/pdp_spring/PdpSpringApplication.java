dc9b21191694ddfadb41a2a0c33194cc208b9bc0
m.olimpiiev <m.olimpiiev@whirl.sg>
2019-06-11 18:16:58 +0300

    Implement first bean injection.

--- src/main/java/sg/whirl/pdp_spring/PdpSpringApplication.java
+++ src/main/java/sg/whirl/pdp_spring/PdpSpringApplication.java
@@ -1,13 +1,30 @@
 package sg.whirl.pdp_spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+import sg.whirl.pdp_spring.beans.AutowiredBean;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
+@Service
 public class PdpSpringApplication {
+
+    @Autowired
+    private AutowiredBean fieldAutowiredBean;
+
+    @PostConstruct
+    void initialized() {
+        if (fieldAutowiredBean != null) {
+            System.out.println("fieldAutowiredBean initialized.");
+        }
+    }
 
     public static void main( String[] args ) {
         SpringApplication.run( PdpSpringApplication.class, args );
+
     }
 
 }
