b7b1215f152b4ae98c293a0995510e6ec74d0f8a
molimpiiev <m.olimpiiev@whirl.sg>
2020-01-01 22:20:16 +0200

    Activate "dev" profile in tests.

--- src/test/java/sg/whirl/pdp_spring/PdpSpringApplicationTests.java
+++ src/test/java/sg/whirl/pdp_spring/PdpSpringApplicationTests.java
@@ -3,10 +3,13 @@ package sg.whirl.pdp_spring;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import sg.whirl.pdp_spring.main.PdpSpringApplication;
 
 @RunWith( SpringRunner.class )
-@SpringBootTest
+@SpringBootTest(classes = PdpSpringApplication.class)
+@ActiveProfiles("dev")
 public class PdpSpringApplicationTests {
 
     @Test
